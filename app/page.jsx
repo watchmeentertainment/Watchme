e9738d78a2b25c4e23368529cebb49cdc6da052c
Watchme <ramkumarramani0604@gmail.com>
2025-09-17 07:04:56 +0530

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import WatchmeMockup from '../components/WatchmeMockup'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <WatchmeMockup />
+    </div>
+  )
+}
