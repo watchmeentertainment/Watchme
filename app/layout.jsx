f575739e68a72b8c16166020de247b3b685cfc99
Watchme <ramkumarramani0604@gmail.com>
2025-09-17 07:26:00 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,13 @@
+export const metadata = {
+  title: "Watchme",
+  description: "Upload and share photos & videos easily",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
 
