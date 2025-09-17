7f41709e4e04d62ed12b10d5d0d098d2fb585c46
Watchme <ramkumarramani0604@gmail.com>
2025-09-17 07:33:58 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
-import WatchmeMockup from '../components/WatchmeMockup'
+import WatchmeMockup from "../components/WatchmeMockup";
+import FileUpload from "../components/FileUpload";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 gap-8">
       <WatchmeMockup />
+      <FileUpload />
     </div>
-  )
+  );
 }
