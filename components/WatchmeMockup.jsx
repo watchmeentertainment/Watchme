be5837669373c335904c747ada7144306a96e887
Watchme <ramkumarramani0604@gmail.com>
2025-09-17 07:07:21 +0530

    Create WatchmeMockup.jsx

---
+++ components/WatchmeMockup.jsx
@@ -0,0 +1,8 @@
+export default function WatchmeMockup() {
+  return (
+    <div className="p-8 bg-white shadow-lg rounded-lg text-center">
+      <h1 className="text-2xl font-bold mb-4">Welcome to Watchme!</h1>
+      <p className="text-gray-600">Upload your photos & videos and share easily.</p>
+    </div>
+  )
+}
